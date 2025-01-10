566f0a41a9b4c45fc7e6e4f58493675b7e2df718
Parzival-05 <parzivalwasd@gmail.com>
2025-01-10 20:23:09 +0300

    Add feature extraction

--- src/features/extraction.py
+++ src/features/extraction.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Callable, Generic, TypeVar
 
+import cv2
+import numpy as np
+
 from src.common import RiceImageType
 from src.features.features_classes import (
     BrightnessFeature,
@@ -29,7 +32,46 @@ class MeasureFeatureExtraction(FeatureExtraction[MeasureFeature]):
         super().__init__(self._extract)
 
     def _extract(self, image: RiceImageType) -> MeasureFeature:
-        return MeasureFeature(width=3, height=4)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+
+        if not contours:
+            raise RuntimeError("There is no rice on image")
+
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        rect = cv2.minAreaRect(largest_contour)
+        angle = rect[2]
+
+        if angle < -45:
+            angle += 90
+
+        center = tuple(map(int, rect[0]))
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(
+            image, M, (image.shape[1], image.shape[0]), flags=cv2.INTER_CUBIC
+        )
+
+        gray_rotated = cv2.cvtColor(rotated, cv2.COLOR_BGR2GRAY)
+        _, thresh_rotated = cv2.threshold(gray_rotated, 50, 255, cv2.THRESH_BINARY)
+        contours_rotated, _ = cv2.findContours(
+            thresh_rotated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+
+        if not contours_rotated:
+            raise RuntimeError("There is no rice on image")
+
+        new_contour = max(contours_rotated, key=cv2.contourArea)
+        _, _, w, h = cv2.boundingRect(new_contour)
+
+        height = max(w, h)
+        width = min(w, h)
+        return MeasureFeature(width=width, height=height)
 
 
 class BrightnessFeatureExtraction(FeatureExtraction[BrightnessFeature]):
@@ -37,7 +79,23 @@ class BrightnessFeatureExtraction(FeatureExtraction[BrightnessFeature]):
         super().__init__(self._extract)
 
     def _extract(self, image: RiceImageType) -> BrightnessFeature:
-        return BrightnessFeature(average_brightness=30.0)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        if not contours:
+            return BrightnessFeature(average_brightness=0.0)
+        rice_contour = max(contours, key=cv2.contourArea)
+
+        V = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[:, :, 2]
+        mask = np.zeros_like(V, dtype=np.uint8)
+        cv2.drawContours(mask, [rice_contour], -1, 255, thickness=cv2.FILLED)  # type: ignore
+
+        rice_pixels = V[mask == 255]
+        average_brightness = float(np.mean(rice_pixels))
+        return BrightnessFeature(average_brightness=average_brightness)
 
 
 class SizeFeatureExtraction(FeatureExtraction[SizeFeature]):
@@ -45,7 +103,18 @@ class SizeFeatureExtraction(FeatureExtraction[SizeFeature]):
         super().__init__(self._extract)
 
     def _extract(self, image: RiceImageType) -> SizeFeature:
-        return SizeFeature(area=30.0)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        if not contours:
+            return SizeFeature(area=0.0)
+        rice_contour = max(contours, key=cv2.contourArea)
+
+        rice_area = cv2.contourArea(rice_contour)
+        return SizeFeature(area=rice_area)
 
 
 class MeasureBrightnessFeatureExtraction(FeatureExtraction[MeasureBrightnessFeature]):
