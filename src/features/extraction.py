b94aa508b54d47de8eef766b32c6675ca00efb1d
Parzival-05 <parzivalwasd@gmail.com>
2025-01-10 18:27:17 +0300

    Add mock of feature extraction

---
+++ src/features/extraction.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Generic, TypeVar
+
+from src.common import RiceImageType
+from src.features.features_classes import (
+    BrightnessFeature,
+    Feature,
+    MeasureBrightnessFeature,
+    MeasureFeature,
+)
+
+T = TypeVar("T", bound=Feature)
+
+
+class FeatureExtraction(ABC, Generic[T]):
+    def __init__(self, extractor: Callable[[RiceImageType], T]):
+        self._extractor = extractor
+
+    @abstractmethod
+    def _extract(self, image: RiceImageType) -> T: ...
+
+    def extract(self, image: RiceImageType) -> T:
+        return self._extractor(image)
+
+
+class MeasureFeatureExtraction(FeatureExtraction[MeasureFeature]):
+    def __init__(self):
+        super().__init__(self._extract)
+
+    def _extract(self, image: RiceImageType) -> MeasureFeature:
+        return MeasureFeature(weight=3, height=4)
+
+
+class BrightnessFeatureExtraction(FeatureExtraction[BrightnessFeature]):
+    def __init__(self):
+        super().__init__(self._extract)
+
+    def _extract(self, image: RiceImageType) -> BrightnessFeature:
+        return BrightnessFeature(average_brightness=30.0)
+
+
+class MeasureBrightnessFeatureExtraction(FeatureExtraction[MeasureBrightnessFeature]):
+    def __init__(
+        self,
+        measure_extractor: FeatureExtraction[MeasureFeature],
+        brightness_extractor: FeatureExtraction[BrightnessFeature],
+    ):
+        super().__init__(self._extract)
+        self.measure_extractor = measure_extractor
+        self.brightness_extractor = brightness_extractor
+
+    def _extract(self, image: RiceImageType) -> MeasureBrightnessFeature:
+        mf = self.measure_extractor.extract(image)
+        bf = self.brightness_extractor.extract(image)
+
+        return MeasureBrightnessFeature(measure=mf, brightness=bf)
